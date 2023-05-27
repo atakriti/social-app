4a8098a93d16859fecc985141ebc33911fefdb21
atakriti <anwart256@gmail.com>
2023-05-27 14:03:24 +0200

    update

--- src/People/People.jsx
+++ src/People/People.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./people.scss";
 import { context } from "../ContextFun";
 import avatar from "../images/avatar.png";
@@ -7,6 +7,8 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
 function People() {
   let { users, findUser } = useContext(context);
+  const [switchMedia, setSwitchMedia] = useState(1);
+
   const handleAddFriend = async (item) => {
     let loggedInUser = doc(db, "users", findUser?.id);
     let selectedUser = doc(db, "users", item?.id);
@@ -15,7 +17,8 @@ function People() {
       let getHisDocument = await getDoc(selectedUser);
       // To get the full document say .data()
       // getHisDocument.data()
-      let hisFriendsRequests =(await getHisDocument.get("friendsRequests")) || [];
+      let hisFriendsRequests =
+        (await getHisDocument.get("friendsRequests")) || [];
       if (hisFriendsRequests.some((it) => it === findUser?.id)) {
         return;
       } else {
@@ -23,7 +26,6 @@ function People() {
         hisFriendsRequests.push(findUser?.id);
         updateDoc(selectedUser, { friendsRequests: hisFriendsRequests });
       }
-     
     } catch (error) {
       alert(error.message);
     }
@@ -31,26 +33,54 @@ function People() {
 
   return (
     <div className="people">
+      <div className="people-headers">
+        <button
+          style={switchMedia === 1 ? { backgroundColor: "gray" } : {}}
+          onClick={() => setSwitchMedia(1)}
+        >
+          People
+        </button>
+        <button
+          style={switchMedia === 2 ? { backgroundColor: "gray" } : {}}
+          onClick={() => setSwitchMedia(2)}
+        >
+          Friends
+        </button>
+      </div>
       <div className="people-container">
-        {users?.filter(item => item.id !== findUser?.id).map((item, i) => (
-          <div key={i} className="singlePeople">
-            <a>
-              <img
-                src={item?.photoURL === null ? avatar : item?.photoURL}
-                alt=""
-              />
-            </a>
-            <h4>{item.displayName}</h4>
-            {item.friendsRequests.some((it) => it === findUser?.id) ? (
-              <button style={{backgroundColor:"gray"}}>Pending</button>
-            ) : (
-              <button onClick={() => handleAddFriend(item)}>
-                <RiUserAddFill />
-                Add friend
-              </button>
-            )}
-          </div>
-        ))}
+        {switchMedia === 2 && findUser.friends.length === 0 && (
+          <h1>There are no friends yet</h1>
+        )}
+
+        {switchMedia === 1 &&
+          users?.filter((item) => item.id !== findUser?.id).map((item, i) => (
+              <div key={i} className="singlePeople">
+                <a>
+                  <img
+                    src={item?.photoURL === null ? avatar : item?.photoURL}
+                    alt=""
+                  />
+                </a>
+                <h4>{item.displayName}</h4>
+                {item.friendsRequests.some((it) => it === findUser?.id) ? (
+                  <button style={{ backgroundColor: "gray" }}>Pending</button>
+                ) : (
+                    findUser.friendsRequests.some(it => it === item.id) ? (
+                        <div className="btns_">
+                            <button className="acc_">Accept</button>
+                            <button className="rej-btn_">Reject</button>
+
+                        </div>
+                    ) : (
+                        <button onClick={() => handleAddFriend(item)}>
+                        <RiUserAddFill />
+                        Add friend
+                      </button>
+                    )
+                 
+                )}
+              </div>
+            ))}
       </div>
     </div>
   );
