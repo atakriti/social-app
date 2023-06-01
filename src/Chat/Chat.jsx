cdcfc0ee3a3c53326ea0ddc7b0e38073433f3835
atakriti <anwart256@gmail.com>
2023-06-01 11:32:17 +0200

    delete fun is done

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./chat.scss";
 import avatar from "../images/avatar.png";
 import { AiOutlineSend } from "react-icons/ai";
+import {BsTrashFill} from "react-icons/bs"
 import {RiArrowGoBackLine} from "react-icons/ri"
 import { context } from "../ContextFun";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
@@ -16,6 +17,7 @@ function Chat() {
    let FullTime = new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes() + ":" + new Date(Date.now()).getSeconds()
  let timeResult = FullDate + "–" + FullTime
   // ============================
+  let scrollRef = useRef()
   const { findUser, users } = useContext(context);
   let friends = users?.filter((user) =>
     findUser?.friends?.some((item) => item === user?.id)
@@ -76,6 +78,36 @@ function Chat() {
     }
   };
 
+
+  const handleDelete = async () => {
+    let selectedUser = doc(db,"users",chatUserId)
+    let loggedInUser = doc(db,"users",findUser?.id)
+    try {
+      let getDocument = await getDoc(selectedUser)
+      let getMyDocument = await getDoc(loggedInUser)
+      let hisChat = await getDocument.get("chat") || []
+      let myChat = await getMyDocument.get("chat") || []
+      // get [my] chats in his chat senderId is the findUser.id
+      let filterHisChat = hisChat.filter(item => item.senderId !== findUser.id)
+      // get [his] chats in my chat senderId is the chatUserId
+      let filterMyChat = myChat.filter(item => item.senderId !== chatUserId)
+
+
+      updateDoc(selectedUser,{chat:filterHisChat})
+      updateDoc(loggedInUser,{chat:filterMyChat})
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+
+  useEffect(() => {
+    // Scroll to the last message when component mounts or combinedMessages changes
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [combinedMessages]);
+
   return (
     <div className="chat">
       {/* Left  */}
@@ -104,6 +136,7 @@ function Chat() {
  <div className="messages">
   <div className="back_btn_container">
   <h5>{users.find(user => user.id === chatUserId).displayName}</h5> 
+   <BsTrashFill className="back_btn" style={{backgroundColor:"red"}} onClick={handleDelete} title="Delete all messages" />
    <RiArrowGoBackLine className="back_btn" onClick={handleBack} title="back" />
   </div>
  {chatUserId === null ? (
@@ -117,6 +150,7 @@ function Chat() {
           <h6>{message.time}</h6>
         </div>
        ))}
+       <div ref={scrollRef}></div>
      </div>
      <form onChange={handleChange} onSubmit={handleSubmit}>
        <input
