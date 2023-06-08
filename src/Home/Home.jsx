82253b7794b042bd626affa1a24610e1374ca6c6
atakriti <anwart256@gmail.com>
2023-06-08 14:17:24 +0200

    update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Mid from "../Mid/Mid";
 import Right from "../Right/Right";
 import "./home.scss";
 function Home() {
+  let noti = () => {
+    Notification.requestPermission().then(prem => {
+      if(prem === "granted"){
+        new Notification(
+          "Example notification",{
+            body:"Hello",
+            
+          }
+        )
+      }
+    })
+  }
+useEffect(() => {
+  noti()
+},[])
+
+
+
+
   return (
     <div className="home">
       <div className="home-container">
