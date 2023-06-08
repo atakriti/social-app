4d0225fff366ef7d20014b02612a562940e1255f
atakriti <anwart256@gmail.com>
2023-06-08 14:32:06 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,50 @@ function App() {
   let location = useLocation();
   let {isLoading,user} = useContext(context)
 
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service worker registered:', registration);
+        })
+        .catch(error => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
+  }
+  window.addEventListener('install', event => {
+    console.log('Service worker installed');
+  });
+  
+  window.addEventListener('activate', event => {
+    console.log('Service worker activated');
+  });
+  
+  window.addEventListener('push', event => {
+    const title = 'Example Notification';
+    const options = {
+      body: 'Hello from your PWA!',
+      // Add additional options as needed
+    };
+  
+    event.waitUntil(
+      window.registration.showNotification(title, options)
+    );
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="home">
       {isLoading && (
