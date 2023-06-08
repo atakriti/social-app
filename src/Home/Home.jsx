ea55827311f91023f5416d11e4f6e9251fdd24dc
atakriti <anwart256@gmail.com>
2023-06-08 14:30:46 +0200

    update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,21 +3,38 @@ import Mid from "../Mid/Mid";
 import Right from "../Right/Right";
 import "./home.scss";
 function Home() {
-  let noti = () => {
-    Notification.requestPermission().then(prem => {
-      if(prem === "granted"){
-        new Notification(
-          "Example notification",{
-            body:"Hello",
-            
-          }
-        )
-      }
-    })
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service worker registered:', registration);
+        })
+        .catch(error => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
   }
-useEffect(() => {
-  noti()
-},[])
+  window.addEventListener('install', event => {
+    console.log('Service worker installed');
+  });
+  
+  window.addEventListener('activate', event => {
+    console.log('Service worker activated');
+  });
+  
+  window.addEventListener('push', event => {
+    const title = 'Example Notification';
+    const options = {
+      body: 'Hello from your PWA!',
+      // Add additional options as needed
+    };
+  
+    event.waitUntil(
+      window.registration.showNotification(title, options)
+    );
+  });
+  
+
 
 
 
